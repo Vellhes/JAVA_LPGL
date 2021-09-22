91248903c1dfe04b630c45655b94e519dd42571e
Vellhes <61982987+Vellhes@users.noreply.github.com>
2021-09-22 15:38:30 +0200

    Création du projet

---
+++ src/Liste.java
@@ -0,0 +1,64 @@
+import java.io.*;
+
+
+public class Liste {
+	Morse morse;
+	Liste suite;
+	
+	public Liste() {
+		super();
+	}
+	public Morse getMorse() {
+		return morse;
+	}
+	public void setMorse(Morse morse) {
+		this.morse = morse;
+	}
+	public Liste getSuite() {
+		return suite;
+	}
+	public void setSuite(Liste suite) {
+		this.suite = suite;
+	}
+	public Liste(Morse morse, Liste suite) {
+		super();
+		this.morse = morse;
+		this.suite = suite;
+	}
+	
+	@Override
+	public String toString() {
+		return "Morse=" + morse + ", suite=" + suite + "";
+	}
+	
+	public static Liste creerListe() throws IOException {
+		File file = new File("src/morse.txt");    
+	      // Créer l'objet File Reader
+	      FileReader fr = new FileReader(file);  
+	      // Créer l'objet BufferedReader        
+	      BufferedReader br = new BufferedReader(fr);      
+	      String ligne;
+	      Liste liste = null;
+	      Liste p = null;
+	      while((ligne = br.readLine()) != null)
+	      {
+	    	char lettre = ligne.charAt(0);
+	    	ligne = ligne.substring(4);
+	        Morse morse = new Morse(lettre,ligne);
+	        liste = new Liste(morse,p);
+	        p=liste;
+	      }
+	      fr.close();
+		return liste;  
+	}
+	
+	public static String recupMorse(char c) throws IOException {
+		Liste liste = creerListe();
+		c=Character.toUpperCase(c);
+		while(liste.morse.lettre != c) {
+			liste = liste.suite;
+		}
+		return liste.morse.code;
+	}
+	
+}
