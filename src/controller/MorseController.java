1e53167bf612ddde80d57a632d431344c47e0641
Vellhes <61982987+Vellhes@users.noreply.github.com>
2021-09-28 18:51:15 +0200

    Réorganisation des fichiers

--- src/controller/MorseController.java
+++ src/controller/MorseController.java
@@ -1,93 +1,107 @@
-//package controller;
-//import java.io.IOException;
-//import java.net.URL;
-//import java.util.ResourceBundle;
-//
-//import javafx.fxml.FXML;
-//import javafx.fxml.Initializable;
-//import javafx.scene.control.Button;
-//import javafx.scene.control.Label;
-//import javafx.scene.control.TextField;
-//import main.Trad;
-//
-//public class MorseController implements Initializable{
-//
-//    @FXML
-//    private Button btn_morse;
-//
-//    @FXML
-//    private Button btn_txt;
-//
-//    @FXML
-//    private TextField tf_trad;
-//
-//    @FXML
-//    private Label lbl_langage;
-//
-//    @FXML
-//    private Label lbl_trad;
-//
-//    @FXML
-//    private Label lbl_condition;
-//    
-//    @FXML
-//    private Button btn_trad;
-//
-//    private String txtAMorse="Vous traduisez du texte au morse";
-//    
-//    private String morseATxt="Vous traduisez du morse au texte";
-//    
-//    private String conditionTxtAMorse="Veuillez ne pas utiliser de caractères spéciaux (?,!,%,/,etc..), uniquement des lettres hors accent et cédille";
-//    
-//    private String conditionMorseATxt="Veuillez séparer chaque code morse par un caractère '/' sans placer d'espace";
-//    
-//	@Override
-//	public void initialize(URL arg0, ResourceBundle arg1) {
-//		
-//		btn_morse.setDisable(true);
-//		btn_txt.setDisable(false);
-//		lbl_langage.setText(txtAMorse);
-//		lbl_condition.setText(conditionTxtAMorse);
-//		lbl_trad.setText("");
-//		tf_trad.clear();
-//		
-//	}
-//	
-//	@FXML
-//	public void choixMorse() {
-//		
-//		btn_morse.setDisable(true);
-//		btn_txt.setDisable(false);
-//		lbl_langage.setText(txtAMorse);
-//		lbl_condition.setText(conditionTxtAMorse);
-//		lbl_trad.setText("");
-//		tf_trad.clear();
-//		
-//	}
-//	
-//	@FXML
-//	public void choixTxt() {
-//	
-//		btn_morse.setDisable(false);
-//		btn_txt.setDisable(true);
-//		lbl_langage.setText(morseATxt);
-//		lbl_condition.setText(conditionMorseATxt);
-//		lbl_trad.setText("");
-//		tf_trad.clear();
-//	}
-//	
-//	@FXML
-//	public void trad() throws IOException {
-//		if(btn_morse.isDisabled()==true && btn_txt.isDisabled()==false) {
-//			lbl_trad.setText(Trad.texteToMorse(tf_trad.getText()));
-//		}
-//		if(btn_morse.isDisabled()==false && btn_txt.isDisabled()==true) {
-//			lbl_trad.setText(Trad.morseToTexte(tf_trad.getText()));
-//		}
-//	}
-//	
-//	
-//
-//    
-//    
-//}
+package controller;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import metiers.Arbre;
+import metiers.Liste;
+import metiers.Trad;
+
+
+public class MorseController implements Initializable{
+
+    @FXML
+    private Button btn_morse;
+
+    @FXML
+    private Button btn_txt;
+
+    @FXML
+    private TextField tf_trad;
+
+    @FXML
+    private Label lbl_langage;
+
+    @FXML
+    private Label lbl_trad;
+
+    @FXML
+    private Label lbl_condition;
+    
+    @FXML
+    private Button btn_trad;
+
+    private String txtAMorse="Vous traduisez du texte au morse";
+    
+    private String morseATxt="Vous traduisez du morse au texte";
+    
+    private String conditionTxtAMorse="Veuillez ne pas utiliser de caractères spéciaux (?,!,%,/,etc..), uniquement des lettres hors accent et cédille";
+    
+    private String conditionMorseATxt="Veuillez séparer chaque code morse par un caractère '/' sans placer d'espace";
+    
+    private Liste liste = null;
+    
+    private Arbre arbre = null;
+    		
+    		
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		try {
+			liste = Liste.creerListe();
+			arbre = Trad.creerArbreMorse("", liste);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		btn_morse.setDisable(true);
+		btn_txt.setDisable(false);
+		lbl_langage.setText(txtAMorse);
+		lbl_condition.setText(conditionTxtAMorse);
+		lbl_trad.setText("");
+		tf_trad.clear();
+		
+	}
+	
+	@FXML
+	public void choixMorse() {
+		
+		btn_morse.setDisable(true);
+		btn_txt.setDisable(false);
+		lbl_langage.setText(txtAMorse);
+		lbl_condition.setText(conditionTxtAMorse);
+		lbl_trad.setText("");
+		tf_trad.clear();
+		
+	}
+	
+	@FXML
+	public void choixTxt() {
+	
+		btn_morse.setDisable(false);
+		btn_txt.setDisable(true);
+		lbl_langage.setText(morseATxt);
+		lbl_condition.setText(conditionMorseATxt);
+		lbl_trad.setText("");
+		tf_trad.clear();
+	}
+	
+	@FXML
+	public void trad() throws IOException {
+		if(btn_morse.isDisabled()==true && btn_txt.isDisabled()==false) {
+			lbl_trad.setText(Trad.texteToMorse(tf_trad.getText()));
+		}
+		if(btn_morse.isDisabled()==false && btn_txt.isDisabled()==true) {
+			lbl_trad.setText(Trad.morseToTexte(tf_trad.getText(),arbre));
+		}
+	}
+	
+	
+
+    
+    
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -2,14 +2,24 @@ package main;
 
 import java.io.IOException;
 
-public class Main{
-//	@Override
-//	public void start(Stage primaryStage) throws Exception {
-//		Parent root = FXMLLoader.load(getClass().getResource("../fxml/Morse.fxml"));
-//		primaryStage.setTitle("Traducteur Mores");
-//		primaryStage.setScene(new Scene(root,600,400));
-//		primaryStage.show();
-//	}
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import metiers.Arbre;
+import metiers.Liste;
+import metiers.Morse;
+import metiers.Trad;
+
+public class Main extends Application{
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("../fxml/Morse.fxml"));
+		primaryStage.setTitle("Traducteur Mores");
+		primaryStage.setScene(new Scene(root,600,400));
+		primaryStage.show();
+	}
 	
 	public static void main(String[] args) throws IOException {
 		
