bc9bf3343f9b50b32bb5fc9e26db21e529b52064
Vellhes <61982987+Vellhes@users.noreply.github.com>
2021-09-28 18:53:10 +0200

    Delete main.java

--- src/main.java
+++
@@ -1,9 +0,0 @@
-import java.io.IOException;
-
-public class main {
-
-	public static void main(String[] args) throws IOException {
-		System.out.println(Trad.texteToMorse("Voici du morse"));
-	     
-	}
-}
